be5e008750ac175effbdeaa969016bdbbf2d5a2b
jordy <marcelo2005jmsp@gmail.com>
2025-02-13 14:08:54 -0500

    metodos GestorProductos creados

--- GestionProductos/src/main/java/ec/edu/espoch/gestionproductos/modelo/GestorProductos.java
+++ GestionProductos/src/main/java/ec/edu/espoch/gestionproductos/modelo/GestorProductos.java
@@ -9,5 +9,40 @@ package ec.edu.espoch.gestionproductos.modelo;
  * @author jordy
  */
 public class GestorProductos {
+    int tamanoVector = 10;
+    Producto[] productos = new Producto[tamanoVector];
     
+    public String agregarProductos(Producto producto){
+        int cont=0;
+        String msg= "Ingreso exitoso";
+        for (int i = 0; i < productos.length; i++) {
+            if (productos[i]== null) {
+                productos[i].setId(i++);
+                productos[i].setNombre(producto.getNombre());
+                productos[i].setPrecio(producto.getPrecio());
+                productos[i].setDisponible(producto.isDisponible());
+            }else{
+                cont++;
+            }    
+        }
+        if (cont==productos.length) {
+            msg="El vector esta lleno";
+        }
+        return msg;
+    }
+    
+    
+    public Producto[] listarProductos(){
+        
+        Producto[] auxProductos = new Producto[tamanoVector];
+        for (int i = 0; i < productos.length; i++) {
+            if (productos[i].isDisponible()) {
+                auxProductos[i].setId(productos[i].getId());
+                auxProductos[i].setNombre(productos[i].getNombre());
+                auxProductos[i].setPrecio(productos[i].getPrecio());
+                auxProductos[i].setDisponible(productos[i].isDisponible());
+            }
+        }
+        return auxProductos;
+    }
 }
--- GestionProductos/src/main/java/ec/edu/espoch/gestionproductos/modelo/Producto.java
+++ GestionProductos/src/main/java/ec/edu/espoch/gestionproductos/modelo/Producto.java
@@ -9,5 +9,49 @@ package ec.edu.espoch.gestionproductos.modelo;
  * @author jordy
  */
 public class Producto {
+    private int id;
+    private String nombre;
+    private float precio;
+    private boolean disponible;
+
+    public Producto(int id, String nombre, float precio, boolean disponible) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.disponible = disponible;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
     
 }
