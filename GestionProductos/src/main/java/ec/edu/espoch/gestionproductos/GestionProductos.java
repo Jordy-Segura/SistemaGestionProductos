3153e13b947868c9b053d8fc87368ab97cc8aedd
jordy <marcelo2005jmsp@gmail.com>
2025-02-13 13:24:42 -0500

    repositorio creado

---
+++ GestionProductos/src/main/java/ec/edu/espoch/gestionproductos/GestionProductos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.gestionproductos;
+
+/**
+ *
+ * @author jordy
+ */
+public class GestionProductos {
+
+    public static void main(String[] args) {
+        
+    }
+}
